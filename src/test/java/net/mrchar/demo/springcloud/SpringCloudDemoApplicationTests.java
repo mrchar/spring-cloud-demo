eec09a29d6c6a7840bc9b18f57f3274eca13bad2
mrchar <mrchar@qq.com>
2022-07-05 15:23:10 +0800

    初始化项目

---
+++ src/test/java/net/mrchar/demo/springcloud/SpringCloudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package net.mrchar.demo.springcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
