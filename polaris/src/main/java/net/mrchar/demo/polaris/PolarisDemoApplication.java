f99d939964643c7009110ab9b77a84a8869ee558
mrchar <mrchar@qq.com>
2022-07-06 13:30:43 +0800

    添加polaris

---
+++ polaris/src/main/java/net/mrchar/demo/polaris/PolarisDemoApplication.java
@@ -0,0 +1,12 @@
+package net.mrchar.demo.polaris;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolarisDemoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PolarisDemoApplication.class, args);
+  }
+}
---
+++ polaris/src/test/java/net/mrchar/demo/polaris/PolarisDemoApplicationTests.java
@@ -0,0 +1,11 @@
+package net.mrchar.demo.polaris;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolarisDemoApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
